232a769c1ebc92ea8019aaf583e3a8b7af55b6e9
Agata Grzanka <40041136+agrzanka@users.noreply.github.com>
2019-03-14 11:58:57 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,2 +1,20 @@
 #include "Cell.h"
 #include "Matrix.h"
+
+Cell::Cell(int x, int y)
+{
+  this.x=x;
+  this.y=y;
+  this.life=false;
+}
+
+//void Cell::getNumOfNeighbors()
+bool Cell:isAlive()
+{
+  return this.life;
+}
+
+void Cell::updateCell(bool l)
+{
+  this.life=l;
+}
