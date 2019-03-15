408a93b2bf476283f8ad99cdbbfdb5d057b505f8
Agata Grzanka <40041136+agrzanka@users.noreply.github.com>
2019-03-15 16:40:35 +0100

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,16 +1,18 @@
 #pragma once
 class Cell
 {
-  private:
-    bool life;
-    int neighborsAlive; 
-  public:
-    Cell(int x, int y);
-    int x;
-    int y;
- 
- // int getNumOfNeighbors();
-    bool isAlive();
-    int getNeighborsAlive();
-    void updateCell(bool l);
+private:
+	bool life;
+	int neighborsAlive;
+public:
+	Cell(int x, int y);
+	Cell();
+	int x;
+	int y;
+
+	// int getNumOfNeighbors();
+	bool isAlive();
+	int getNeighborsAlive();
+	void setNeighborsAlive(int n);
+	void updateCell(bool l);
 };
